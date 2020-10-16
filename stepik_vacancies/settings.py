c499b7c1049c4e11bf6998213a007491eca653c5
Andrey <andreyamaterasu@gmail.com>
2020-10-16 22:18:10 +0500

    del static fil

--- stepik_vacancies/settings.py
+++ stepik_vacancies/settings.py
@@ -119,7 +119,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'media')
+    os.path.join(BASE_DIR, 'media/company_images')
 ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
