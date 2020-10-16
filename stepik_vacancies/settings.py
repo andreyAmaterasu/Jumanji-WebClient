390b8c39701480676cf93a7805df4eca8434f277
Andrey <andreyamaterasu@gmail.com>
2020-10-16 15:44:33 +0500

    edit settings

--- stepik_vacancies/settings.py
+++ stepik_vacancies/settings.py
@@ -118,6 +118,12 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
