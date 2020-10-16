1f572152cbc15e19ff0597c12e0eba5593d99cdb
Andrey <andreyamaterasu@gmail.com>
2020-10-16 22:44:35 +0500

    del static f

--- stepik_vacancies/urls.py
+++ stepik_vacancies/urls.py
@@ -42,6 +42,8 @@ urlpatterns = [
 handler404 = custom_handler404
 handler500 = custom_handler500
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL,
+#                           document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
