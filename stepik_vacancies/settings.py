10186fdc21014afb357e3b5e6acfad1723c7b39c
Andrey <andreyamaterasu@gmail.com>
2020-10-16 23:26:48 +0500

    da

--- stepik_vacancies/settings.py
+++ stepik_vacancies/settings.py
@@ -124,7 +124,7 @@ STATICFILES_DIRS = [
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-MEDIA_URL = '/static/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MEDIA_COMPANY_IMAGE_DIR = 'company_images'
